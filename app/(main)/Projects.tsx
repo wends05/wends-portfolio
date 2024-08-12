7b66b85e4730071907280ede4b69ae387d97a727
wends05 <cxnner05@gmail.com>
2024-08-13 04:42:51 +0800

    incomplete parts

--- app/(main)/Projects.tsx
+++ app/(main)/Projects.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div>
+      <div className="h-80 w-full bg-gradient-to-t from-neutral-700" />
+      <div className="h-screen px-4 bg-neutral-700">
+        <h3>What I&apos;ve made so far</h3>
+      </div>
+      <div className="h-80 w-full bg-gradient-to-b from-neutral-700" />
+     
+    </div>
+  );
 };
 
 export default Projects;

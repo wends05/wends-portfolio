b6b43a3acc029f2bfa6eba59269ca7e3a66f8d2f
wends05 <cxnner05@gmail.com>
2024-08-13 23:06:54 +0800

    sample about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,16 +3,27 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <div className="flex flex-col h-screen w-full items-center justify-center">
+      <div className="flex h-screen w-full flex-col items-center justify-center">
         <h1>About</h1>
         <p>this website</p>
       </div>
       <div className="p-4">
-        <p>
+        <p>Hi!</p>
+        <div>
+          This project lasted me 2 months. I started it last May 2024, because I
+          already had knowledge of how to create websites / web applications
+          with React, which is the current standard library (along with Next.js)
+          for making SPAs. Knowing also that in the future, I&apos;ll be using
+          this same portfolio for applying for jobs, why not make it now, and
+          then eventually update this as I go through college.
+        </div>
 
-
-          Hi!
-        </p>
+        <div>
+          This project was only supposed to be finished in development for like
+          a week or so. But I really had no clear idea on what to write on my
+          portfolio, and considering my work style is very messy, It got delayed
+          until August of 2024, which is when I finally finished this project.
+        </div>
       </div>
     </div>
   );

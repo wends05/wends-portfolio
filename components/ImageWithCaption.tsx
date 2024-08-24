0ee549a55e46560eebefc4b6d340ee8889a7b0f9
wends05 <cxnner05@gmail.com>
2024-08-24 16:23:46 +0800

    about page area

---
+++ components/ImageWithCaption.tsx
@@ -0,0 +1,43 @@
+'use client';
+import Image, { StaticImageData } from "next/image";
+import { motion } from 'framer-motion'
+
+interface ImageWithCaptionProps {
+  src: StaticImageData;
+  className: string;
+  caption: string;
+  imageClassName?: string;
+}
+
+const ImageWithCaption = ({
+  src,
+  className,
+  caption,
+  imageClassName
+}: ImageWithCaptionProps) => {
+  return (
+    <motion.div
+    initial={{
+      opacity: 0,
+      y: 50,
+    }}
+    whileInView={{
+      opacity: 1,
+      y: 0,
+    }}
+
+    transition={{
+      duration: 0.5,
+      ease: "easeOut"
+    }}
+    
+    className={"flex flex-col py-2"}>
+      <div className={"relative " + className}>
+        <Image src={src} alt="figma1" fill className={"object-cover " + imageClassName} />
+      </div>
+      <p className="text-sm">{caption}</p>
+    </motion.div>
+  );
+};
+
+export default ImageWithCaption;

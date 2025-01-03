3c96c7d5a52efd3eb9924cbe726a160dafc1c323
wends05 <cxnner05@gmail.com>
2025-01-03 20:50:46 +0800

    test lint part 2

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,9 @@
-import Image from "next/image";
-import figma1 from "@/public/about/figma1.jpg";
-import figma2 from "@/public/about/figma2.jpg";
-
-import wireframe1 from "@/public/about/wireframe1.jpg";
-import wireframe2 from "@/public/about/wireframe2.jpg";
-
-import content1 from "@/public/about/content1.jpg";
-import content2 from "@/public/about/content2.jpg";
-import ImageWithCaption from "@/components/ImageWithCaption";
+"use client";
 const About = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center space-y-8">
       <h1>About</h1>
+      <p>sample page</p>
     </div>
   );
 };

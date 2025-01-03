6f8d130f88e16a81c4402d255b1bb2e176ff2fc0
wends05 <cxnner05@gmail.com>
2025-01-03 20:19:42 +0800

    i forgot what this is: Added transition between pages

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import TransitionLink from "./TransitionLink";
 
 const Navbar = () => {
   return (
     <nav className="fixed z-50 flex w-full items-center justify-between p-4">
-      <Link href={"/"}>
+      <TransitionLink title="Home" href={"/"}>
         <div className="h-10 w-10 bg-primary-500"></div>
-      </Link>
-      <Link href={"/about"} className="">
+      </TransitionLink>
+      <TransitionLink href={"/about"} title="About" className="">
         about
-      </Link>
+      </TransitionLink>
     </nav>
   );
 };

38b4babf8077e441ca05475b9bfe2893a939e6f0
wends05 <cxnner05@gmail.com>
2025-01-05 00:56:48 +0800

    fixed navbar, removed skills area (temp), added projects page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import TransitionLink from "./TransitionLink";
+import { HomeIcon } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <nav className="fixed z-50 flex w-full items-center justify-between p-4">
-      <TransitionLink title="Home" href={"/"}>
-        <div className="h-10 w-10 bg-primary-500"></div>
+    <nav className="fixed z-50 flex w-full items-center justify-between p-4 pt-5">
+      <TransitionLink title="Home" href={"/"} className="">
+        <HomeIcon />
       </TransitionLink>
       <TransitionLink href={"/about"} title="About" className="">
         about
